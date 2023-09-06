df10ab761154d868ddae618d17ef2a5943af2da0
VKryukov <kv-xlab@yandex.ru>
2023-09-06 18:19:07 +0500

    Фикс максимального размера шапки

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,29 +14,31 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <button
-        className={clsx(styles.header__logo, styles.animatedText)}
-        onClick={logoClickHandler}
-      >
-        Todos
-      </button>
-
-      <div className={styles.socials}>
-        <a
-          href={'mailto: vprosolupov1@gmail.com'}
-          className={clsx(styles.link, styles.animatedText)}
-          target={'_blank'}
+      <div className={styles.header__content}>
+        <button
+          className={clsx(styles.header__logo, styles.animatedText)}
+          onClick={logoClickHandler}
         >
-          Email
-        </a>
+          Todos
+        </button>
 
-        <a
-          href={'https://t.me/F3ckingRain'}
-          className={clsx(styles.link, styles.animatedText)}
-          target={'_blank'}
-        >
-          Telegram
-        </a>
+        <div className={styles.socials}>
+          <a
+            href={'mailto: vprosolupov1@gmail.com'}
+            className={clsx(styles.link, styles.animatedText)}
+            target={'_blank'}
+          >
+            Email
+          </a>
+
+          <a
+            href={'https://t.me/F3ckingRain'}
+            className={clsx(styles.link, styles.animatedText)}
+            target={'_blank'}
+          >
+            Telegram
+          </a>
+        </div>
       </div>
     </header>
   );
